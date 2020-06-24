11112c92d0b87730807ea8bf3596af33b9e057e0
Andrey Orlov <andrey@endpointclosing.com>
2020-06-23 23:37:03 -0700

    added another route

--- canvas-server/main.go
+++ canvas-server/main.go
@@ -31,8 +31,6 @@ func handleConnection(connection net.Conn){
 	defer connection.Close()
 	// read request
 	request(connection)
-	// write request
-	respond(connection)
 }
 
 func request(connection net.Conn){
@@ -42,11 +40,7 @@ func request(connection net.Conn){
 		line := scanner.Text()
 		fmt.Println(line)
 		if i == 0 {
-			// request line
-			method := strings.Fields(line)[0]
-			url := strings.Fields(line)[1]
-			fmt.Println("****METHOD", method)
-			fmt.Println("****URL", url)
+			multiPlexer(connection, line)
 		}
 		if line == "" {
 			break
@@ -55,8 +49,23 @@ func request(connection net.Conn){
 	}
 }
 
-// write response 
-func respond(connection net.Conn){
+func multiPlexer(connection net.Conn, line string){
+				// request line
+				method := strings.Fields(line)[0]
+				url := strings.Fields(line)[1]
+				fmt.Println("****METHOD", method)
+				fmt.Println("****URL", url)
+
+				if method == "GET" && url == "/" {
+					index(connection)
+				}
+				if method == "GET" && url =="/about" {
+					about(connection)
+				}
+
+}
+
+func index(connection net.Conn){
 	body := `
 	<!-- DOCTYPE html--->
 	<html lang="en">
@@ -103,6 +112,78 @@ func respond(connection net.Conn){
 	  
 	  if (arc.fill) {
 		ctx.fillStyle = arc.color;
+		ctx.fill();
+	  } else {
+		ctx.strokeStyle = arc.color;
+		ctx.lineWidth = arc.stroke;
+		ctx.stroke();
+	  }
+	  if (i < 2000) {
+		setTimeout(function(){
+		  drawCircle();
+		}, 10);
+	  }
+	}
+	
+	drawCircle();
+	}
+	</script>
+	</html>`
+	fmt.Fprint(connection, "HTTP/1.1 200 OK \r\n")
+	fmt.Fprint(connection, "Content-Type: text/html \r\n")	
+	fmt.Fprint(connection, "\r\n")
+	fmt.Fprint(connection,	body)	
+}
+
+func about(connection net.Conn){
+	body := `
+	<!-- DOCTYPE html--->
+	<html lang="en">
+	<body class="main">
+	<canvas id="canvas" style="height:100%; width:100%;"></canvas>
+	</body>
+	<script  type="text/javascript">
+	console.log("here")
+
+
+	window.onload = function(){
+		console.log("here 1")
+		const b = document.getElementsByClassName('main')[0]
+		b.style.background = "white"
+		let c = document.getElementById('canvas');
+	let ctx = c.getContext('2d');
+	let i = 0;
+	
+	ctx.canvas.width = window.innerWidth;
+	ctx.canvas.height = window.innerHeight;
+	ctx.canvas.style.background = '#'+Math.floor(Math.random()*16777215).toString(16);
+	ctx.canvas.style.position = 'fixed';
+	
+	let min = (canvas.width < canvas.height) ? canvas.width : canvas.height;
+	
+	function drawCircle() {
+	  i++;
+	  let weight = Math.pow(Math.random(), 2);
+	  let seed1 = Math.floor(Math.floor(weight * ((min / 2) - 0 + 1)));
+	  let seed2 = Math.floor(Math.random() * canvas.height);
+	  let seed3 = Math.floor(Math.random() * canvas.width);
+	  let primary = '#'+Math.floor(Math.random()*16777215).toString(16);
+	  let secondary = '#'+Math.floor(Math.random()*16777215).toString(16);
+	  let arc = {
+		x: seed3 - (seed1 / 2),
+		y: seed2 - (seed1 / 2),
+		r: seed1,
+		shadow: !(Math.random()+0.5|0),
+		blur: Math.random() * (10 + 1),
+		color: (!(Math.random()+0.5|0)) ? primary : secondary,
+		stroke: Math.random() * (15 + 1),
+		fill: !(Math.random()+0.5|0)
+	  };
+	  ctx.beginPath();
+	  ctx.arc(arc.x, arc.y, arc.r, Math.PI * 0, Math.PI * 2);
+	  
+	  if (arc.fill) {
+		ctx.fillStyle = "#00000";
 		ctx.fill();
 	  } else {
 		ctx.strokeStyle = arc.color;
